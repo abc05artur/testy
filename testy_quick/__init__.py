08a059e3dfbb9925d3b116021d69c68aba19f77b
artur <Artur.Baboi-ext@pernod-ricard.com>
2023-01-24 06:47:40 +0100

    c 0.1.1

--- testy_quick/__init__.py
+++ testy_quick/__init__.py
@@ -3,4 +3,4 @@ from .end_functions import create_test_case, run_test_case_unsafe as run_test_ca
 from .user_string import user_set_option, set_overwrite_test_results, set_test_exists_function
 from .variable_handlers import register_handler, SingleHandler, MultiHandler
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
